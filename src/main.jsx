a286a6cf3bd66e9db1430ff394818976270e3f66
sayed115 <sayedmohamed11577@gmail.com>
2024-10-20 10:03:47 +0300

    Activities

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
+
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
